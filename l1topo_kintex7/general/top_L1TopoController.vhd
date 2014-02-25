57d7f970efc55d5acdd058e8a5e845d9528aab83
edma <edma@edma-aspire.(none)>
2014-02-25 20:20:25 +0100

    sync

--- l1topo_kintex7/general/top_L1TopoController.vhd
+++ l1topo_kintex7/general/top_L1TopoController.vhd
@@ -391,8 +391,17 @@ SIM_CLOCK: if SIMULATION generate
     idelayctrl_refclk300 <= '0';
     wait for 1.666 ns;
   end process DELAY_CLK;
+
+  process
+  begin  -- process
+    gck2_clk80 <= '1'; 
+    wait for 6.25 ns;
+    gck2_clk80 <= '0';
+    wait for 6.25 ns;
+  end process;
   rst_ipb <= '0';
   gck2_mmcm_locked <= not rst_from_bank18;
+  soft_rst <= '0';
 end generate SIM_CLOCK;
         -----------------------------------------------------------------------
         -- comment for sim
@@ -445,167 +454,167 @@ end generate SIM_CLOCK;
 ------ Ethernet MAC core and PHY interface
 ----
                          
-	eth: entity work.eth_7s_sgmii
-		port map(
-			gt_clkp => gt_clkp,
-			gt_clkn => gt_clkn,
-			gt_txp => gt_txp,
-			gt_txn => gt_txn,
-			gt_rxp => gt_rxp,
-			gt_rxn => gt_rxn,
---			sig_detn => sfp_los,
-			clk125_out => clk125,
-			clk125_fr => clk125_fr,
-			rsti => rst_eth,
-			locked => eth_locked,
-			tx_data => mac_tx_data,
-			tx_valid => mac_tx_valid,
-			tx_last => mac_tx_last,
-			tx_error => mac_tx_error,
-			tx_ready => mac_tx_ready,
-			rx_data => mac_rx_data,
-			rx_valid => mac_rx_valid,
-			rx_last => mac_rx_last,
-			rx_error => mac_rx_error,
-			pcs_pma_status => pcs_pma_status,
-			ExternalPhyChip_reset_out => phy_reset
-		);
+--	eth: entity work.eth_7s_sgmii
+--		port map(
+--			gt_clkp => gt_clkp,
+--			gt_clkn => gt_clkn,
+--			gt_txp => gt_txp,
+--			gt_txn => gt_txn,
+--			gt_rxp => gt_rxp,
+--			gt_rxn => gt_rxn,
+----			sig_detn => sfp_los,
+--			clk125_out => clk125,
+--			clk125_fr => clk125_fr,
+--			rsti => rst_eth,
+--			locked => eth_locked,
+--			tx_data => mac_tx_data,
+--			tx_valid => mac_tx_valid,
+--			tx_last => mac_tx_last,
+--			tx_error => mac_tx_error,
+--			tx_ready => mac_tx_ready,
+--			rx_data => mac_rx_data,
+--			rx_valid => mac_rx_valid,
+--			rx_last => mac_rx_last,
+--			rx_error => mac_rx_error,
+--			pcs_pma_status => pcs_pma_status,
+--			ExternalPhyChip_reset_out => phy_reset
+--		);
       
-	PHY_RESET_OUT_N <= not phy_reset;
+--	PHY_RESET_OUT_N <= not phy_reset;
       
--- ipbus control logic
+---- ipbus control logic
 
-	ipbus: entity work.ipbus_ctrl
-		port map(
-			mac_clk => clk125,
-			rst_macclk => rst_125,
-			ipb_clk => gck2_clk40, --ipb_clk,
-			rst_ipb => rst_ipb,
-			mac_rx_data => mac_rx_data,
-			mac_rx_valid => mac_rx_valid,
-			mac_rx_last => mac_rx_last,
-			mac_rx_error => mac_rx_error,
-			mac_tx_data => mac_tx_data,
-			mac_tx_valid => mac_tx_valid,
-			mac_tx_last => mac_tx_last,
-			mac_tx_error => mac_tx_error,
-			mac_tx_ready => mac_tx_ready,
-			ipb_out => ipb_master_out,
-			ipb_in => ipb_master_in,
-			mac_addr => mac_addr,
-			ip_addr => ip_addr,
-			pkt_rx => pkt_rx,
-			pkt_tx => pkt_tx,
-			pkt_rx_led => pkt_rx_led,
-			pkt_tx_led => pkt_tx_led
-		);
+--	ipbus: entity work.ipbus_ctrl
+--		port map(
+--			mac_clk => clk125,
+--			rst_macclk => rst_125,
+--			ipb_clk => gck2_clk40, --ipb_clk,
+--			rst_ipb => rst_ipb,
+--			mac_rx_data => mac_rx_data,
+--			mac_rx_valid => mac_rx_valid,
+--			mac_rx_last => mac_rx_last,
+--			mac_rx_error => mac_rx_error,
+--			mac_tx_data => mac_tx_data,
+--			mac_tx_valid => mac_tx_valid,
+--			mac_tx_last => mac_tx_last,
+--			mac_tx_error => mac_tx_error,
+--			mac_tx_ready => mac_tx_ready,
+--			ipb_out => ipb_master_out,
+--			ipb_in => ipb_master_in,
+--			mac_addr => mac_addr,
+--			ip_addr => ip_addr,
+--			pkt_rx => pkt_rx,
+--			pkt_tx => pkt_tx,
+--			pkt_rx_led => pkt_rx_led,
+--			pkt_tx_led => pkt_tx_led
+--		);
       	
 
-	mac_addr <= X"000A3501F610";
-	--ip_addr <= X"865D828B"; --134.93.130.139
-	ip_addr <= X"898A5114"; --137.138.81.20
+--	mac_addr <= X"000A3501F610";
+--	--ip_addr <= X"865D828B"; --134.93.130.139
+--	ip_addr <= X"898A5114"; --137.138.81.20
 
 
--- ipbus slaves live in the entity below, and can expose top-level ports
--- The ipbus fabric is instantiated within.
+---- ipbus slaves live in the entity below, and can expose top-level ports
+---- The ipbus fabric is instantiated within.
 
-	slaves: entity work.slaves port map(
-		ipb_clk => gck2_clk40, --ipb_clk
-		ipb_rst => rst_ipb,
-		ipb_in => ipb_master_out,
-		ipb_out => ipb_master_in,
-		rst_out => sys_rst,
-		pkt_rx => pkt_rx,
-		pkt_tx => pkt_tx,
+--	slaves: entity work.slaves port map(
+--		ipb_clk => gck2_clk40, --ipb_clk
+--		ipb_rst => rst_ipb,
+--		ipb_in => ipb_master_out,
+--		ipb_out => ipb_master_in,
+--		rst_out => sys_rst,
+--		pkt_rx => pkt_rx,
+--		pkt_tx => pkt_tx,
 
-      mac_addr <= X"000A3501F610";
-      --ip_addr <= X"865D828B"; --134.93.130.139
-      ip_addr <= X"898A5114"; --137.138.81.20
+--      mac_addr <= X"000A3501F610";
+--      --ip_addr <= X"865D828B"; --134.93.130.139
+--      ip_addr <= X"898A5114"; --137.138.81.20
 
 
--- ipbus slaves live in the entity below, and can expose top-level ports
--- The ipbus fabric is instantiated within.
+---- ipbus slaves live in the entity below, and can expose top-level ports
+---- The ipbus fabric is instantiated within.
 
-      slaves: entity work.slaves 
-      generic map(
-      	lvds_lines => LINKS_NUMBER
-      	)
-      port map(
-      	ipb_clk => gck2_clk40, --ipb_clk
-      	ipb_rst => rst_ipb,
-      	ipb_in => ipb_master_out,
-      	ipb_out => ipb_master_in,
-      	rst_out => sys_rst,
-      	pkt_rx => pkt_rx,
-      	pkt_tx => pkt_tx,
+--      slaves: entity work.slaves 
+--      generic map(
+--      	lvds_lines => LINKS_NUMBER
+--      	)
+--      port map(
+--      	ipb_clk => gck2_clk40, --ipb_clk
+--      	ipb_rst => rst_ipb,
+--      	ipb_in => ipb_master_out,
+--      	ipb_out => ipb_master_in,
+--      	rst_out => sys_rst,
+--      	pkt_rx => pkt_rx,
+--      	pkt_tx => pkt_tx,
 
       	
-		ipb_write_U1_out => ipb_write_U1,
-		ipb_read_U1_in => ipb_read_U1,
-		ipb_write_U2_out => ipb_write_U2,
-		ipb_read_U2_in => ipb_read_U2,
+--		ipb_write_U1_out => ipb_write_U1,
+--		ipb_read_U1_in => ipb_read_U1,
+--		ipb_write_U2_out => ipb_write_U2,
+--		ipb_read_U2_in => ipb_read_U2,
       	
-		ctrlbus_idelay_value_out => ctrlbus_idelay_value,
-		ctrlbus_idelay_load_out => ctrlbus_idelay_load,
+--		ctrlbus_idelay_value_out => ctrlbus_idelay_value,
+--		ctrlbus_idelay_load_out => ctrlbus_idelay_load,
       	
       	
-			soft_rst_out => soft_rst,
+--			soft_rst_out => soft_rst,
       		
-		DBG_STATE_IN     => dbg_ddr_state,
-		DBG_REG_DATA_IN  => dbg_ddr_reg,
-		DBG_BITSLIP_IN   => dbg_ddr_bitslip,
-		DBG_INC_IN       => dbg_ddr_inc,
-		DBG_PAUSE_IN     => dbg_ddr_pause,
-		DBG_STEP_IN      => dbg_ddr_step,
+--		DBG_STATE_IN     => dbg_ddr_state,
+--		DBG_REG_DATA_IN  => dbg_ddr_reg,
+--		DBG_BITSLIP_IN   => dbg_ddr_bitslip,
+--		DBG_INC_IN       => dbg_ddr_inc,
+--		DBG_PAUSE_IN     => dbg_ddr_pause,
+--		DBG_STEP_IN      => dbg_ddr_step,
       	
-		ROD_RAM_CLK_IN => gck2_clk80,
-		ROD_RAM_WE_IN => ram_we,
-		ROD_RAM_ADDR_IN => ram_addr,
-		ROD_RAM_DATA_IN => ram_data
-	);
+--		ROD_RAM_CLK_IN => gck2_clk80,
+--		ROD_RAM_WE_IN => ram_we,
+--		ROD_RAM_ADDR_IN => ram_addr,
+--		ROD_RAM_DATA_IN => ram_data
+--	);
       
       
-	move : entity work.from_rod_to_ipbus
-	port map(
-		clk => gck2_clk80,
-		reset => rst_ipb,
+--	move : entity work.from_rod_to_ipbus
+--	port map(
+--		clk => gck2_clk80,
+--		reset => rst_ipb,
       	
-		parsers_data_in => rod_data,
-		parsers_rd_out => rod_re,
-		parsers_rdy_in => rod_rdy,
+--		parsers_data_in => rod_data,
+--		parsers_rd_out => rod_re,
+--		parsers_rdy_in => rod_rdy,
       	
-		ram_we_out => ram_we,
-		ram_waddr_out => ram_addr,
-		ram_data_out => ram_data
-	);
+--		ram_we_out => ram_we,
+--		ram_waddr_out => ram_addr,
+--		ram_data_out => ram_data
+--	);
       
-	ctrlbus: entity work.ctrlbus
-		port map(
-			gck2_clk40_in => gck2_clk40,
-			gck2_clk80_in => gck2_clk80,
-			idelayctrl_refclk300_in => idelayctrl_refclk300,
-			gck2_mmcm_locked_in => gck2_mmcm_locked,
-			CTRLBUS_U1_OUT_P => CTRLBUS_U1_OUT_P,
-			CTRLBUS_U1_OUT_N => CTRLBUS_U1_OUT_N,
-			CTRLBUS_U2_OUT_P => CTRLBUS_U2_OUT_P,
-			CTRLBUS_U2_OUT_N => CTRLBUS_U2_OUT_N,
-			CTRLBUS_U1_IN_P => CTRLBUS_U1_IN_P,
-			CTRLBUS_U1_IN_N => CTRLBUS_U1_IN_N,
-			CTRLBUS_U2_IN_P => CTRLBUS_U2_IN_P,
-			CTRLBUS_U2_IN_N => CTRLBUS_U2_IN_N,
-			ipb_write_U1_in => ipb_write_U1,
-			ipb_read_U1_out => ipb_read_U1,
-			ipb_write_U2_in => ipb_write_U2,
-			ipb_read_U2_out => ipb_read_U2,
-			idelay_value_in => ctrlbus_idelay_value,
-			idelay_load_in => ctrlbus_idelay_load,
+--	ctrlbus: entity work.ctrlbus
+--		port map(
+--			gck2_clk40_in => gck2_clk40,
+--			gck2_clk80_in => gck2_clk80,
+--			idelayctrl_refclk300_in => idelayctrl_refclk300,
+--			gck2_mmcm_locked_in => gck2_mmcm_locked,
+--			CTRLBUS_U1_OUT_P => CTRLBUS_U1_OUT_P,
+--			CTRLBUS_U1_OUT_N => CTRLBUS_U1_OUT_N,
+--			CTRLBUS_U2_OUT_P => CTRLBUS_U2_OUT_P,
+--			CTRLBUS_U2_OUT_N => CTRLBUS_U2_OUT_N,
+--			CTRLBUS_U1_IN_P => CTRLBUS_U1_IN_P,
+--			CTRLBUS_U1_IN_N => CTRLBUS_U1_IN_N,
+--			CTRLBUS_U2_IN_P => CTRLBUS_U2_IN_P,
+--			CTRLBUS_U2_IN_N => CTRLBUS_U2_IN_N,
+--			ipb_write_U1_in => ipb_write_U1,
+--			ipb_read_U1_out => ipb_read_U1,
+--			ipb_write_U2_in => ipb_write_U2,
+--			ipb_read_U2_out => ipb_read_U2,
+--			idelay_value_in => ctrlbus_idelay_value,
+--			idelay_load_in => ctrlbus_idelay_load,
       		
-			mmcm_clk_80_u1_out => ctrlbus_32_clk,
-			mmcm_clk_400_u1_out => ctrlbus_32_clkx8,
+--			mmcm_clk_80_u1_out => ctrlbus_32_clk,
+--			mmcm_clk_400_u1_out => ctrlbus_32_clkx8,
       		
-			mmcm_clk_80_u2_out => ctrlbus_17_clk,
-			mmcm_clk_400_u2_out => ctrlbus_17_clkx8
-		);
+--			mmcm_clk_80_u2_out => ctrlbus_17_clk,
+--			mmcm_clk_400_u2_out => ctrlbus_17_clkx8
+--		);
 -------------------------------------------------------------------------------
 -- end comment for sim
 -------------------------------------------------------------------------------
